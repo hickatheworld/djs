8a242ac154b66b9d513f91e3e173488e7f8c4c86
Hicka <hello@hicka.dev>
2021-06-27 13:23:45 +0200

    style: chain .get listeners

--- index.js
+++ index.js
@@ -33,64 +33,65 @@ https.get('https://raw.githubusercontent.com/discordjs/discord.js/docs/stable.js
 server.set('view engine', 'ejs');
 server.set('views', './views');
 
-server.get('/', function (_req, res) {
-	res.redirect('https://discord.js.org/#/');
-});
-server.get('/favicon.ico', function (_req, res) {
-	res.sendStatus(404);
-});
-server.get('/*', function (req, res) {
-	const params = req.params[0].split('.');
-	const base = params.shift();
-	if (!(base in docs.classes || base in docs.typedefs))
-		return res.redirect(`${BASE_DOCS_URL}/search?query=${base}.${params.join('.')}`);
-	if (params.length === 0) {
-		if (base in docs.classes)
-			return res.redirect(`${BASE_DOCS_URL}/classes/${base}`);
-		else
-			return res.redirect(`${BASE_DOCS_URL}/typedef/${base}`);
-	}
-	let current = docs.typedefs[base] || docs.classes[base];
-	let scroll;
-	// Using every instead of forEach allows to 'break' the loop by returning false, just like using break.
-	params.every((param, i) => {
-		if (current.props.some(p => p.name === param)) {
-			if (i === params.length - 1)
-				scroll = param;
-			else {
-				let type = current.props.find(p => p.name === param).type.flat();
-				if (type.length > 1) {
+server
+	.get('/', function (_req, res) {
+		res.redirect('https://discord.js.org/#/');
+	})
+	.get('/favicon.ico', function (_req, res) {
+		res.sendStatus(404);
+	})
+	.get('/*', function (req, res) {
+		const params = req.params[0].split('.');
+		const base = params.shift();
+		if (!(base in docs.classes || base in docs.typedefs))
+			return res.redirect(`${BASE_DOCS_URL}/search?query=${base}.${params.join('.')}`);
+		if (params.length === 0) {
+			if (base in docs.classes)
+				return res.redirect(`${BASE_DOCS_URL}/classes/${base}`);
+			else
+				return res.redirect(`${BASE_DOCS_URL}/typedef/${base}`);
+		}
+		let current = docs.typedefs[base] || docs.classes[base];
+		let scroll;
+		// Using every instead of forEach allows to 'break' the loop by returning false, just like using break.
+		params.every((param, i) => {
+			if (current.props.some(p => p.name === param)) {
+				if (i === params.length - 1)
 					scroll = param;
-					return false;
+				else {
+					let type = current.props.find(p => p.name === param).type.flat();
+					if (type.length > 1) {
+						scroll = param;
+						return false;
+					}
+					current = docs.typedefs[type[0][0]] || docs.classes[type[0][0]];
 				}
-				current = docs.typedefs[type[0][0]] || docs.classes[type[0][0]];
-			}
 
-		} else if (current.methods.some(p => p.name === param)) {
-			scroll = param;
-			return false;
+			} else if (current.methods.some(p => p.name === param)) {
+				scroll = param;
+				return false;
+			}
+			else
+				return false;
+
+			return true;
+		});
+		const scope = (current.name in docs.classes) ? 'class' : 'typedef';
+		let url = `${BASE_DOCS_URL}/${scope}/${current.name}`;
+		let desc = current.description;
+		if (scroll) {
+			url += `?scrollTo=${scroll}`;
+			const prop = current.props.find(p => p.name === scroll) || current.methods.find(m => m.name === scroll);
+			desc = prop.description;
+			if (prop.returns)
+				desc += `\nReturn type: ${prop.nullable ? '?' : ''}${prop.returns.flat().flat().join('').replace(/</g, '&#60;').replace(/>/g, '&#62;')}`;
+			else if (prop.type)
+				desc += `\nType: ${prop.nullable ? '?' : ''}${prop.type.flat().flat().join('').replace(/</g, '&#60;').replace(/>/g, '&#62;')}`;
+
+		}
+		if (req.headers['user-agent'].includes('Discordbot')) {
+			res.render('embed', { name: `${base}.${params.join('.')}`, desc });
 		}
 		else
-			return false;
-
-		return true;
+			res.redirect(url);
 	});
-	const scope = (current.name in docs.classes) ? 'class' : 'typedef';
-	let url = `${BASE_DOCS_URL}/${scope}/${current.name}`;
-	let desc = current.description;
-	if (scroll) {
-		url += `?scrollTo=${scroll}`;
-		const prop = current.props.find(p => p.name === scroll) || current.methods.find(m => m.name === scroll);
-		desc = prop.description;
-		if (prop.returns)
-			desc += `\nReturn type: ${prop.nullable ? '?' : ''}${prop.returns.flat().flat().join('').replace(/</g, '&#60;').replace(/>/g, '&#62;')}`;
-		else if (prop.type)
-			desc += `\nType: ${prop.nullable ? '?' : ''}${prop.type.flat().flat().join('').replace(/</g, '&#60;').replace(/>/g, '&#62;')}`;
-
-	}
-	if (req.headers['user-agent'].includes('Discordbot')) {
-		res.render('embed', { name: `${base}.${params.join('.')}`, desc });
-	}
-	else
-		res.redirect(url);
-});
